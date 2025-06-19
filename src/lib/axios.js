19e4263e39b5a29750586a22d368e934bac3e399
Anthony <anthonywongjw@gmail.com>
2025-06-19 13:40:34 -0400

    feat: Connect frontend to deployed backend API

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://taskproductivity-api.onrender.com/api"
+      : "http://localhost:5001/api",
   withCredentials: true,
 });
